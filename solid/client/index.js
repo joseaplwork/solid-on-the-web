0a58898e66c931ff45cce48e65a900b9e5fb83ca
Jose Paredes <joseaplwork@gmail.com>
2021-05-14 21:18:02 +0200

    Identity who are the agents of change

--- solid/client/index.js
+++ solid/client/index.js
@@ -1,36 +1,43 @@
+// View/Design logic -- Designers
 function setupClickHandlers() {
     const buttonEl = document.querySelector('.done');
  
     buttonEl.addEventListener('click',  onButtonClicked);   
 }
 
+// Change behiavou, what does done do? -- Product manager
+// Change of copy -- Copywriting 
 function onButtonClicked() {
     const name = getFullName();
     const firstName = parseFirstName(name);
 
-    renderMessage(firstName);
+    displayWelcome(firstName);
 }
 
+// View change -- Designers, PM, Developers
 function getFullName() {
     return document.querySelector('.name');
 }
 
+// QA / Customer service
 function parseFirstName(fullName) {
     if (
         fullName[0] === 'Mr.' ||
         fullName[0] === 'Mrs.' ||
         fullName[0] === 'Dr.'
     ) {
-        fullName[1];        
+        return fullName[1];        
     }
 
     return fullName[0];
 }
 
-function renderMessage(name) {
+// UI/View -- Designers. Copywriting
+function displayWelcome(name) {
     const welcomEl = document.getElementById('welcome');
 
     welcomEl.innerHTML = `Welcome ${name}!`;
 }
 
+// loading code -- Developers
 window.addEventListener(setupClickHandlers);
