3978a957901baab4516eefc13e0028e641377b45
Jose Paredes <joseaplwork@gmail.com>
2021-06-04 02:33:57 +0200

    Open Close Principiple before Refactored

--- solid/client/paint/PaintReport.js
+++ solid/client/paint/PaintReport.js
@@ -2,70 +2,98 @@ export class PaintReport {
     constructor(data, options = {}) {
         this.data = data;
         this.reverseColumns = !!options.reverseColumns;
+        this.prices = options.prices;
+        this.useDivs = !!options.useDivs;
     }
 
     eachColor(fn) {
         const colors = Object.keys(this.data);
 
-        colors.forEach((color) => {
+        return colors.map((color) => {
             const remaining = this.data[color];
 
-            fn(color, remaining);
+            return fn(color, remaining);
         });
     }
 
     generate() {
-        this.report = this.getReportHeader();
-
-        this.eachColor((color, remaining) => {
-            this.report += this.getReportRow(color, remaining);
-        });
-
-        this.report += `
-                </tbody>
-            </table>
-        `;
-
-        return this.report;
+        return this.wrapReport(
+            this.getReportHeader() +
+            this.wrapBody(
+                this.eachColor((color, remaining) => {
+                    return this.getReportRow(color, remaining);
+                }).join('')
+            )
+        );
     }
 
     getReportHeader() {
-        if (this.reverseColumns) {
-            return `
-            <table>
-                <thead>
-                    <tr>
-                        <th>Remaining</th>
-                        <th>Color</th>
-                    </tr>
-                </thead>
-                <tbody>`;
-        }
-        
-        return `
-            <table>
-                <thead>
-                    <tr>
-                        <th>Color</th>
-                        <th>Remaining</th>
-                    </tr>
-                </thead>
-                <tbody>`;
+        let columns = ['Color', 'Remaining'];
+
+        if (this.prices) columns.push('Price');
+        if (this.reverseColumns) columns = columns.reverse();
+
+        const ths = columns.map((c) => this.wrapColumn(c)).join('');
+
+        return this.wrapHeader(ths);
     }
 
     getReportRow(color, remaining) {
-        if (this.reverseColumns) {
-            return `
-                    <tr>
-                        <td>${remaining}</td>
-                        <td>${color}</td>
-                    </tr>`;
+        let values = [color, remaining];
+
+        if (this.prices) values.push(this.prices[color]);
+        if (this.reverseColumns) values = values.reverse();
+
+        const tds = values.map((v) => this.wrapValue(v)).join('');
+
+        return this.wrapRow(tds);
+    }
+
+    wrapReport(content) {
+        if (this.useDivs) {
+            return `<div class="table">${content}</div>`;
         }
 
-        return `
-                    <tr>
-                        <td>${color}</td>
-                        <td>${remaining}</td>
-                    </tr>`;
+        return `<table>${content}</table>`;
+    }
+
+    wrapBody(content) {
+        if (this.useDivs) {
+            return content;
+        }
+
+        return `<tbody>${content}</tbody>`;
+    }
+    
+    wrapHeader(content) {
+        if (this.useDivs) {
+            return `<div>${content}</div>`;
+        }
+
+        return `<thead><tr>${content}</tr></thead>`;
+    }
+
+    wrapRow(content) {
+        if (this.useDivs) {
+            return `<div>${content}</div>`;
+        }
+
+        return `<tr>${content}</tr>`;
+    }
+
+    wrapValue(content) {
+        if (this.useDivs) {
+            return `<span>${content}</span>`;
+        }
+   
+        return `<td>${content}</td>`;
+    }
+
+    wrapColumn(content) {
+        if (this.useDivs) {
+            return `<strong>${content}</strong>`;
+        }
+   
+        return `<th>${content}</th>`;
     }
 }
--- solid/client/paint/__tests__/paint.spec.js
+++ solid/client/paint/__tests__/paint.spec.js
@@ -7,74 +7,74 @@ describe('Paint', () => {
     });
 
     describe('rendering a report', () => {
-        it('returns an HTML table', () => {
-            const paint = new Paint(new PaintStore());
+        let paint, prices;
+
+        beforeEach(() => {
+            paint = new Paint(new PaintStore());
+            prices = {
+                blue: 100,
+                red: 200,
+                green: 300,
+            }
 
             paint.usePaint('blue', 1);
             paint.usePaint('red', 2);
             paint.usePaint('green', 3);
+        });
+        it('returns an HTML table', () => {
+            const expected = '<table><thead>' + 
+                '<tr><th>Color</th><th>Remaining</th></tr>' +
+                '</thead><tbody>' +
+                '<tr><td>blue</td><td>7</td></tr>'+
+                '<tr><td>red</td><td>6</td></tr>'+
+                '<tr><td>green</td><td>5</td></tr>'+
+                '</tbody></table>';
 
-            const expected = `
-            <table>
-                <thead>
-                    <tr>
-                        <th>Color</th>
-                        <th>Remaining</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>blue</td>
-                        <td>7</td>
-                    </tr>
-                    <tr>
-                        <td>red</td>
-                        <td>6</td>
-                    </tr>
-                    <tr>
-                        <td>green</td>
-                        <td>5</td>
-                    </tr>
-                </tbody>
-            </table>`;
-
-            expect(paint.generateReport().trim()).toEqual(expected.trim());
+            expect(paint.generateReport()).toEqual(expected);
         });
 
         it('reverses the order of the columns', () => {
-            const paint = new Paint(new PaintStore());
-
-            paint.usePaint('blue', 1);
-            paint.usePaint('red', 2);
-            paint.usePaint('green', 3);
-
-            const expected = `
-            <table>
-                <thead>
-                    <tr>
-                        <th>Remaining</th>
-                        <th>Color</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>7</td>
-                        <td>blue</td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>red</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>green</td>
-                    </tr>
-                </tbody>
-            </table>`;
+            const expected = '<table><thead>' + 
+                '<tr><th>Remaining</th><th>Color</th></tr>' +
+                '</thead><tbody>' +
+                '<tr><td>7</td><td>blue</td></tr>'+
+                '<tr><td>6</td><td>red</td></tr>'+
+                '<tr><td>5</td><td>green</td></tr>'+
+                '</tbody></table>';
 
             expect(paint.generateReport({
                 reverseColumns: true
-            }).trim()).toEqual(expected.trim());
+            })).toEqual(expected);
+        });
+
+        it('includes price information', () => {
+            const expected = '<table><thead>' + 
+                '<tr><th>Price</th><th>Remaining</th><th>Color</th></tr>' +
+                '</thead><tbody>' +
+                '<tr><td>100</td><td>7</td><td>blue</td></tr>'+
+                '<tr><td>200</td><td>6</td><td>red</td></tr>'+
+                '<tr><td>300</td><td>5</td><td>green</td></tr>'+
+                '</tbody></table>';
+        
+            expect(paint.generateReport({
+                reverseColumns: true,
+                prices: prices
+            })).toEqual(expected);
+        });
+
+        it('uses div', () => {
+            const expected = '<div class="table">' + 
+                '<div><strong>Price</strong><strong>Remaining</strong><strong>Color</strong></div>' +
+                '<div><span>100</span><span>7</span><span>blue</span></div>'+
+                '<div><span>200</span><span>6</span><span>red</span></div>'+
+                '<div><span>300</span><span>5</span><span>green</span></div>'+
+                '</div>';
+
+            expect(paint.generateReport({
+                reverseColumns: true,
+                prices: prices,
+                useDivs: true
+            })).toEqual(expected);
         });
     });
 
